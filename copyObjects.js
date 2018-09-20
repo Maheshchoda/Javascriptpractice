f467bde7ab7a8d01eb07266b72f39196bf1d06c7
Maheshchoda <choda.maheshbabu@gmail.com>
2018-09-20 10:25:59 +0530

    Practice with Objects

--- copyObjects.js
+++ copyObjects.js
@@ -16,6 +16,10 @@ console.log(oldFruits);
 console.log(freshFruits);
 
 //for Objects
+//We can copy Objects we can three ways
+// 1) Object.assign()
+// 2) Object.freeze()
+// 3) Object.seal()
 
 let person = { name: `Mahesh`, age: 21};
 // let person2 =person
@@ -24,3 +28,55 @@ person2.name = 'Kotesh';
 
 console.log(person);
 console.log(person2);
+
+//Object.freeze is another method where Freezed object is passed
+//Syntax:-- Object.freeze(ObjectName);
+//What is freeze method???
+  //Actually, it’s just a method that doesn’t allow
+  //us to add new properties to such object,
+  // remove existing ones and change the current prototype of such object
+
+const Bags = {
+  skyBags: 'Good for Travelling',
+  AmericanTourister: 'Good for BackPack',
+  worst:{
+    wildcraft: 'Worst for any kind'
+  },
+  good: {
+    rebook: `It is good for workout`
+  }
+}
+
+const Hari = Object.freeze(Bags);
+console.log(Hari);
+Hari.skyBags = 'It\'s Bad Dude';//new value assigned, but it's not going to effect
+console.log(Hari);
+// but that not case with the nested Object
+Hari.worst.wildcraft = `I like it`;//value is going to be reassigned
+console.log(Hari);
+//In order to prevent that from reassigning
+Object.freeze(Hari.good);
+Hari.good.rebook = `It's not at all good`;//It's not going to be reassigned
+console.log(Hari);
+
+
+//Object.seal() is ????
+          // With sealing, the situation is a little bit different.
+          // We still can’t add new properties, delete existing ones
+          //and every property becomes non-configurable but the values of properties can be changed
+
+
+
+//We can check whether object is frozen or not by using following
+//Object.isFrozen(objectName);
+
+//***** delete *****
+      //delete is an operator where we can delete the particular property
+
+//Some useful methods how to check whether the input is array or not
+//We can't use the typeof so there is builtIn method for that
+
+const arr = ['Banana', 'Manoges', 'PineApple'];
+
+//Syntax: -- Array.isArray(arryName);
+console.log(Array.isArray(arr));
---
+++ objectOrientedProgramming.js
@@ -0,0 +1,81 @@
+//The main idea behind the Object oriented programming is // to
+//*** reduce code duplication
+//and improve redability through class
+
+//Before class we implemented object oriented in ES5
+
+const assert = require('assert');
+
+function person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName  = lastName;
+  this.checkIt   = function (){
+    return this;
+  }
+  return `Hello Mr.${firstName}.${lastName}`;
+}
+
+const Saurabh = new person('Saurabh', 'Wadwha');
+
+assert(Saurabh.checkIt() === Saurabh, `Yeah bro your assumption is true`);
+console.log(Saurabh);
+
+//In ES2015 the above code looks like
+
+class secondPerson {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName  = lastName;
+    this.checkIt = function () {
+      return this;
+    }
+  }
+}
+
+const Yash = new secondPerson('Yash', 'Damani');
+console.log(Yash);
+
+//Explanation for above example
+//Inside the class keyword we are using the special function
+//called the constructor
+//Constructor(from above example):-
+            //Constructor is a special name of function
+            //This function is going to run when the class is invoked using the new keyword
+            //In oop when we invoke or create a object from class then we called it as
+                          //instantiation (or) creation of instance
+
+//If we want to created a method and use the properties of previous function
+//functionname.prototype.property
+//In ES5
+function FirstPerson(firstName, lastName){
+  this.firstName = firstName;
+  this.lastName  = lastName;
+}
+ //creating method for above function
+
+FirstPerson.prototype.age = function () {
+  console.log(`Hello my name is ${firstName} and my age is 23`);
+  return `Hello my name is ${firstName} and my age is 23`;
+}
+FirstPerson.prototype.height = 5.9;
+
+const Krishna = new FirstPerson('Krishna', 'SaiCharan');
+console.log(Krishna.height);
+console.log(Krishna);
+//We are setting the properties dynamically
+
+//The above code is ES2015
+
+class thirdPerson{
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName  = lastName;
+  }
+    interests() { //we are placing the prototype methods inside of class
+      return `I am intersted to watch anime`;
+  }
+}
+
+const Mahesh = new thirdPerson('Mahesh', 'Choda');
+console.log(Mahesh.interests());
+console.log(Mahesh);
