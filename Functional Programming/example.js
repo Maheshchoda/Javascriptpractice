0b6f1680e5abbbe579e7e8f3fc0ed960261cde41
Maheshchoda <choda.maheshbabu@gmail.com>
2019-01-26 22:16:28 +0530

    some prac

--- Functional Programming/example.js
+++ Functional Programming/example.js
@@ -31,3 +31,35 @@ showStudent(4434 - 334);
 
 //3) It can potentially throw an error if the student isn't found, which causes the
 // entire program stack to unwind and end abruptly.
+
+//We will improve the above code to more FP later, as of now we can make two
+// simple enhancements
+
+// Seperate the long function into shorter functions, each with a single purpose.
+
+//Reduce the number of side effects by explicitly defining all arguments needed
+// for the function to carry out their job.
+
+//find function need a reference to the object store and id of student to look up.
+const find = curry(function(db, id) {
+  const obj = db.get(id);
+  if (obj === null) throw new Error("Object not found");
+  return obj;
+});
+
+//convert a student object into comma seperated values
+const csv = function(student) {
+  `${student.ssn}, ${student.firstName}, ${student.lastName}`;
+};
+
+//to display a student details on the page, you need the
+// elementId and student id
+
+const append = curry(function(elementId, info) {
+  document.querySelector(elementId).innerHTML = info;
+});
+
+//partially sets the HTML elementId to use in the function
+
+const showStudent = run(append("#student-info"), csv, find(db));
+showStudent(44430 - 23423);
